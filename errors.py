2aacdf5856974e5aa5101a212a4637573e8062ad
Rafael Freitas <rafael@ezsolucoes.com.br>
2022-11-19 22:36:01 -0300

    Refactoring exercises

--- errors.py
+++ errors.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python3
 
-names = open("names.txt").readlines()
 
-print(names[2])
+import os
+import sys
+import time
+import logging
+
+log = logging.Logger("errors")
+
+def try_to_open_a_file(filepath, attempts=1) -> list:
+    """Tries to open a file, if error, retries n times"""
+    for attempt in range(1,attempts +1):
+        try:
+            return open(filepath).readlines()
+        except FileNotFoundError as e:
+            log.error("ERRO: %s",e)
+            time.sleep(1)
+        else:
+            print("Success")
+        finally:
+            print("Execute this always")
+    return []
+
+
+for line in try_to_open_a_file("names.txt", attempts=5):
+    print(line)
+
+# try:    
+#     print(names[1])
+# except:
+#     print("[Error] Missing name in the list")
+#     sys.exit(1)
